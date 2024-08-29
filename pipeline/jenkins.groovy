19aaa2dd76a927543d28a89fe5a3b5882cd1f004
vitali-o <165700266+vitali-o@users.noreply.github.com>
2024-08-29 14:43:09 +0000

    add docker login section

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,6 +1,12 @@
 pipeline {
     agent any
     
+    environment {
+        // Define environment variables
+        REGISTRY = "vitaliio"
+        DOCKER_CREDENTIALS_ID = "dockerhub-credentials"
+    }
+
     parameters {
         choice(name: 'OS', choices: ['linux', 'android', 'windows'], description: 'Pick OS')
         choice(name: 'ARCH', choices: ['386', 'arm', 'arm64', 'amd64'], description: 'Pick ARCH')
@@ -31,8 +37,12 @@ pipeline {
             }
         }
         stage('push to dockerhub') {
-            steps { 
+            steps {
                 script {
+                        // Log in to Docker Hub (or another Docker registry)
+                        withCredentials([usernamePassword(credentialsId: "${DOCKER_CREDENTIALS_ID}", usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                        sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin ${REGISTRY}"
+                    }
                     echo "Push image to dockerhub"
                     sh "make push"
                 }
